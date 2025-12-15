0495675d39e1c137c6557871224a95770fea1cb6
irkode <irkode@rikode.de>
2025-12-15 13:03:01 +0100

    feat(build) simplified build cli

--- build.ps1
+++ build.ps1
@@ -1,83 +1,101 @@
-[CmdLetBinding()]
+[CmdLetBinding(DefaultParameterSetName)]
 param(
   # Skip some of the default steps
   [Parameter(Mandatory = $false)][ValidateSet(
     'BuildHighlightJS',
+    'BuildDiscoursePlugin',
+    'BuildHighlightJSPlugin',
     'CloneHighlightJS',
+    'DeveloperBuild',
     'GenerateHugoModules',
-    'GenerateModules',
     'UpdateHugoDocs',
-    'TestHighlightJS',
-    'DeveloperBuild'
+    'Distribute',
+    'updateHugoDocs',
+    'ShowStatus',
+    'TestHighlightJS'
   )][string[]]$Skip,
-  # Force some actions that else might be skipped based on project state
+  # Force actions that else might be skipped based on project state
   [Parameter(Mandatory = $false)][ValiDateSet('SetupHighlightJS')][string[]]$Force,
   # Build only extra languages
   [Parameter(Mandatory = $false)][ValiDateSet('true','false')][string]$OnlyExtra = 'true',
   # Proceed regardless of test failures
   [Parameter(Mandatory = $false)][switch]$IgnoreMarkupErrors,
-  # only build developer.html for visual testing
-  [Parameter(Mandatory = $false)][switch]$DevBuildOnly,
-  # just check for changes
-  [Parameter(Mandatory = $false)][switch]$StatusOnly,
   # publish to Distribution folder
   [Parameter(Mandatory = $false)][Alias('Dist')][switch]$Distribute,
-  # only distribute last results
-  [Parameter(Mandatory = $false)][Alias('DistOnly')][switch]$DistributeOnly
+  [Parameter(Mandatory = $false)][ValidateSet(
+    'BuildHighlightJS',
+    'BuildDiscoursePlugin',
+    'BuildHighlightJSPlugin',
+    'CloneHighlightJS',
+    'DeveloperBuild',
+    'GenerateHugoModules',
+    'UpdateHugoDocs',
+    'Distribute',
+    'ShowStatus',
+    'updateHugoDocs'
+    )][string[]]$Steps
 )
 
 $startCWD = Get-Location
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
-
-try {
-  $ScriptsDir = Resolve-Path (Join-Path -Path $PSScriptRoot -ChildPath "scripts")
-  . (Join-Path -Path $ScriptsDir "lib\utilities.ps1")
-  . (Join-Path -Path $ScriptsDir "lib\build-functions.ps1")
-
-  $ProjectRoot = Test-Folder $PSScriptRoot
-  $WorkDir = Test-Folder -Create $ProjectRoot "work"
-  $HugoDocsDir = Test-Folder $ProjectRoot "hugoDocs"
-  $HugoGenDir = Test-Folder $ScriptsDir "hugen"
-  $DistributionDir = Test-Folder -Create $ProjectRoot "dist"
-
-  $HighlightJsDir = Join-Path $WorkDir "highlight.js"
-  $HighlightJsExtraDir = Join-Path $HighlightJsDir "extra"
-  Write-Verbose "Starting from: $ProjectRoot"
-  Write-Verbose "Working Directory: $WorkDir"
-} catch {
-  Write-Error "$_`nInitialization failed!" -ErrorAction Continue
-  throw $_
-} finally {
-  Set-Location $StartCWD
+If ($PSBoundParameters.Keys -notcontains 'Steps') {
+  $Steps = @(
+    'updateHugoDocs', 'cloneHighlightJS',
+    'generateHugoModules',
+    'buildHighlightJS', 'buildDiscoursePlugin', 'buildHighlightJSPlugin',
+    'developerBuild'
+  )
 }
+if ($PSBoundParameters.Keys -contains 'Skip') {
+  $Steps = $steps | Where-Object { $Skip -notcontains $_}
+}
+if ($Distribute) { $Steps += 'distribute'}
 
 try {
-  & $ScriptsDir/versions-set-wanted.ps1 -VersionConfigFile $ProjectRoot\.versions.json
-  # we expect all external tools be installed before starting a local build
-  & $ScriptsDir/versions-check-installed.ps1 -VersionConfigFile $ProjectRoot\.versions.json
+  try {
+    $ScriptsDir = Resolve-Path (Join-Path -Path $PSScriptRoot -ChildPath "scripts")
+    Import-Module (Join-Path -Path $ScriptsDir -ChildPath "lib/utilities.ps1")
+    Import-Module (Join-Path -Path $ScriptsDir -ChildPath "lib/build-functions.ps1")
+    $ProjectRoot = Test-Folder $PSScriptRoot
+    $WorkDir = Test-Folder -Create $ProjectRoot "work"
+    $HugoDocsDir = Test-Folder $ProjectRoot "hugoDocs"
+    $HugoGenDir = Test-Folder $ScriptsDir "hugen"
+    $DistributionDir = Test-Folder -Create $ProjectRoot "dist"
 
-  if ($StatusOnly) { try { showStatus; exit 0 } catch { throw } }
-  if ($DistributeOnly) { try { distribute; exit 0 } catch { throw } }
-    if (-Not $DevBuildOnly) {
-    updateHugoDocs
-    cloneHighlightJS
-    generateHugoModules
-    buildHighlightJS
-    buildDiscoursePlugin
-    buildHighlightJSPlugin
+    $HighlightJsDir = Join-Path $WorkDir "highlight.js"
+    $HighlightJsExtraDir = Join-Path $HighlightJsDir "extra"
+    Write-Verbose "Starting from: $ProjectRoot"
+    Write-Verbose "Working Directory: $WorkDir"
+  } catch {
+    Write-Error "$_`nInitialization failed!" -ErrorAction Continue
+    throw $_
+  } finally {
+    Set-Location $StartCWD
   }
-  developerBuild
-  if ($Distribute) { distribute }
-  showStatus
-} catch {
-  Write-Error "$_`nBuild failed!" -ErrorAction Continue
-  throw $_
-} finally {
-  Set-Location $StartCWD
-}
 
-Set-Location $startCWD
+  $Steps | Out-Host
+  try {
+    & $ScriptsDir/versions-set-wanted.ps1 -VersionConfigFile $ProjectRoot\.versions.json
+    # we expect all external tools be installed before starting a local build
+    & $ScriptsDir/versions-check-installed.ps1 -VersionConfigFile $ProjectRoot\.versions.json
+
+    foreach ($step in $Steps) {
+      & $step
+    }
+  } catch {
+    Write-Error "$_`nBuild failed!" -ErrorAction Continue
+    throw $_
+  } finally {
+    Set-Location $StartCWD
+  }
+} catch {
+    Write-Error  "$_`nBUILD failed!" -ErrorAction Stop
+} finally {
+  Remove-Module -Force utilities
+  Remove-Module -Force build-functions
+  Set-Location $startCWD
+}
 Write-Host -ForegroundColor Green "Check the Diffs for HugoDocs and highlightjs-hugo"
-Write-Host -ForegroundColor Green "DONE."
+Write-Host -ForegroundColor Green "BUILD successful."
 exit 0
