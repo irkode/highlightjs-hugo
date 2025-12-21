415a089b41bb08379e1f1be934a3dd97ff4ff5f8
irkode <irkode@rikode.de>
2025-12-21 10:27:27 +0100

    dep(): bump Go to 1.25.5, Hugo to 0.153.1
    dep(go) bump Go to 1.25.5
    
    dep(hugo) bump Hugo to 0.153.1
    
    build(hugen): search dependencx files relative to .versions.json

--- scripts/versions-set-wanted.ps1
+++ scripts/versions-set-wanted.ps1
@@ -1,23 +1,25 @@
 [CmdLetBinding()]
 param(
-  [Parameter(Mandatory=$true)][String]$VersionConfigFile = ".versions.json"
+  [Parameter(Mandatory = $true)][String]$VersionConfigFile = ".versions.json"
 )
 [void](Test-Path $VersionConfigFile -ErrorAction Stop)
+$VersionConfigFile = Resolve-Path $VersionConfigFile -ErrorAction Stop
+$rootDir = Split-Path -Parent $VersionConfigFile
 $Versions = Get-Content -Raw -Path $VersionConfigFile | ConvertFrom-Json -AsHashtable
 $Versions.Keys | ForEach-Object {
   $key = $_.Replace('!', '')
-  $source = $Versions[$_]['source']
+  $source = Join-Path -Resolve -Path $rootDir -ChildPath $Versions[$_]['source']
   $version = $Versions[$_]['version']
   $envVarName = $key.ToUpper() + '_VERSION'
   switch ($key) {
-    "go" { $version = (Get-Content $source | select-string "^go (\d+\.\d+\.\d+)").Matches.Groups[1].Value }
-    "hugo" { $version = (Get-Content $source).Replace('v', '')}
+    "go" { $version = (Get-Content $source | Select-String "^go (\d+\.\d+\.\d+)").Matches.Groups[1].Value }
+    "hugo" { $version = (Get-Content $source).Replace('v', '') }
   }
   Write-Verbose ("Set $envVarName to $version$(if ($source) { " [$source]"})")
   if ($env:GITHUB_ACTIONS) {
     "$envVarName=$version" | Add-Content -Encoding utf8 $env:GITHUB_ENV
     switch ($key) {
-      "node" { '{"engines": { "node": "' + $version + '"}}' | Set-Content -encoding utf8 .node.package.json }
+      "node" { '{"engines": { "node": "' + $version + '"}}' | Set-Content -Encoding utf8 .node.package.json }
     }
   } else {
     [System.Environment]::SetEnvironmentVariable($envVarName, $version)
