edfefb68e31d8b4ec36c794f356ad7c060318c52
irkode <irkode@rikode.de>
2026-04-18 13:31:01 +0200

    docs(all) apply formatting with gotplfmt

--- build/hugen/layouts/_partials/get-javascript.js
+++ build/hugen/layouts/_partials/get-javascript.js
@@ -1,4 +1,4 @@
-{{- warnf "EXEC %s for %s" templates.Current.Name .Page.Path}}
+{{- warnf "EXEC %s for %s" templates.Current.Name .Page.Path }}
 {{- $dataLang := .Params.hljs.keywords }}
 
 {{- $keywords := partialCached "get-keywords.html" .Params.hljs .Params.hljs }}
@@ -9,23 +9,23 @@
 
 {{- /* generate keyword base patterns for the action root modes */ -}}
 {{- range $regexName, $regexWords := index $dataKeywords "patterns" }}
-  {{- $pad.SetInMap "js" $regexName (printf "const re_%s = /%s/;" $regexName (delimit $regexWords "|")) }}
+   {{- $pad.SetInMap "js" $regexName (printf "const re_%s = /%s/;" $regexName (delimit $regexWords "|")) }}
 {{- end }}
 
 {{- range $scope, $words := $keywords }}
-  {{- range index $dataKeywords.generate $scope }}
-    {{- if eq "kw" . }}
-      {{- $wordsWithRelevance := slice }}
-      {{- range $words }}
-        {{- $wordsWithRelevance = $wordsWithRelevance  | append (printf "'%s'" (cond (hasPrefix . "hugo.") (add . "|10") .)) }}
+   {{- range index $dataKeywords.generate $scope }}
+      {{- if eq "kw" . }}
+         {{- $wordsWithRelevance := slice }}
+         {{- range $words }}
+            {{- $wordsWithRelevance = $wordsWithRelevance | append (printf "'%s'" (cond (hasPrefix . "hugo.") (add . "|10") .)) }}
+         {{- end }}
+         {{- $const := add "kw_" (upper $scope) }}
+         {{- $pad.SetInMap "js" $const (printf "const %s = [%s];" $const (delimit $wordsWithRelevance ",")) }}
+      {{- else if eq "re" . }}
+         {{- $const := add "re_" (upper $scope) }}
+         {{- $pad.SetInMap "js" $const (printf "const %s = /\\b(%s)\\b/;" $const (replace (delimit $words "|") "." `\.`)) }}
       {{- end }}
-      {{- $const := add "kw_" (upper $scope) }}
-      {{- $pad.SetInMap "js" $const (printf "const %s = [%s];" $const (delimit $wordsWithRelevance ",")) }}
-    {{- else if eq "re" . }}
-      {{- $const := add "re_" (upper $scope) }}
-      {{- $pad.SetInMap "js" $const (printf "const %s = /\\b(%s)\\b/;" $const (replace (delimit $words "|") "." `\.`)) }}
-    {{- end }}
-  {{- end }}
+   {{- end }}
 {{- end }}
 {{- return $pad.Values }}
 {{- /* remove all trailing whitespace */ -}}
--- build/hugen/layouts/discourse/section.markdown
+++ build/hugen/layouts/discourse/section.markdown
@@ -3,11 +3,11 @@
 {{- warnf "SEC( MD )  %s - %s" $pagePath $tpl }}
 
 {{- range $lang := .Params.plugins }}
-  {{- range resources.Match "discourse/**" }}
-    {{- warnf "Execute: %s - %s" $lang . }}
-    {{- with resources.ExecuteAsTemplate (printf "discourse/%s/%s" $lang .Name) (dict "lang" $lang) . }}
-      {{- $noop := .Publish }}
-    {{- end }}
-{{- end }}
+   {{- range resources.Match "discourse/**" }}
+      {{- warnf "Execute: %s - %s" $lang . }}
+      {{- with resources.ExecuteAsTemplate (printf "discourse/%s/%s" $lang .Name) (dict "lang" $lang) . }}
+         {{- $noop := .Publish }}
+      {{- end }}
+   {{- end }}
 {{- end }}
 {{- /* remove all trailing whitespace */ -}}
